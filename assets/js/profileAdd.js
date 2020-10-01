392b3bddc7e84549f248f035b1c0ad9f152097c9
Nazmi <nazmifeeroz@hotmail.com>
2020-10-01 21:28:11 +0800

    bug: hides image if profile image link is broken

--- assets/js/profileAdd.js
+++ assets/js/profileAdd.js
@@ -1,39 +1,40 @@
-$.getJSON('https://raw.githubusercontent.com/lugnitdgp/Hack-Day/2020/data.json', (data) => {
-  // console.log(data); // this will show the info it in firebug console
-  let profileKeys = ['handle', 'image_link', 'message'];
-  /**
-   * Check if a profile has handle image_link and message properties
-   */
-  let isProfileValid = (profile) => (profileKeys.every((k) => k in profile));
+$.getJSON(
+  "https://raw.githubusercontent.com/lugnitdgp/Hack-Day/2020/data.json",
+  (data) => {
+    // console.log(data); // this will show the info it in firebug console
+    let profileKeys = ["handle", "image_link", "message"]
+    /**
+     * Check if a profile has handle image_link and message properties
+     */
+    let isProfileValid = (profile) => profileKeys.every((k) => k in profile)
 
-  /**
-   * Given an array of profiles, keep only one for handle (handle is the id of the profile)
-   */
-  let getUniqueProfiles = (profiles) => (
-    Array.from(new Set(profiles.map((p) => p.handle))).map((id) => {
-      let profile = {};
-      profileKeys.forEach((k) => {
-        profile[k] = profiles.find((p) => p.handle === id)[k];
-      });
-      return profile;
-    })
-  );
+    /**
+     * Given an array of profiles, keep only one for handle (handle is the id of the profile)
+     */
+    let getUniqueProfiles = (profiles) =>
+      Array.from(new Set(profiles.map((p) => p.handle))).map((id) => {
+        let profile = {}
+        profileKeys.forEach((k) => {
+          profile[k] = profiles.find((p) => p.handle === id)[k]
+        })
+        return profile
+      })
 
-  // get only unique and valid profiles
-  let profiles = getUniqueProfiles(data.profiles.filter(isProfileValid));
+    // get only unique and valid profiles
+    let profiles = getUniqueProfiles(data.profiles.filter(isProfileValid))
 
-  let cardParent = document.getElementById('profile-cards');
-  for (let index = 0; index < profiles.length; index += 1) {
-    let card = document.createElement('div');
-    card.classList.add('col-12');
-    card.classList.add('col-md-4');
-    card.classList.add('p-3');
+    let cardParent = document.getElementById("profile-cards")
+    for (let index = 0; index < profiles.length; index += 1) {
+      let card = document.createElement("div")
+      card.classList.add("col-12")
+      card.classList.add("col-md-4")
+      card.classList.add("p-3")
 
-    let profile = profiles[index];
-    card.innerHTML = `<div class='card'>
+      let profile = profiles[index]
+      card.innerHTML = `<div class='card'>
             <div class='row no-gutters'>
               <div class='col-4'>
-                <img src='${profile.image_link}' class='card-img' alt='${profile.handle}'>
+                <img src='${profile.image_link}' class='card-img' alt='${profile.handle}' onerror="this.style.display='none'">
               </div>
               <div class='col-8'>
                 <div class='card-body'>
@@ -43,13 +44,15 @@ $.getJSON('https://raw.githubusercontent.com/lugnitdgp/Hack-Day/2020/data.json',
               </div>
             </div>
           </div>
-      `;
-    cardParent.appendChild(card);
+      `
+      cardParent.appendChild(card)
+    }
+    if (!profiles.length) {
+      let card = document.createElement("div")
+      card.classList.add("col-12")
+      card.innerHTML =
+        "<h1>No ninjas encountered yet. Are you one? Feel free to add your card!</h1>"
+      cardParent.append(card)
+    }
   }
-  if (!profiles.length) {
-    let card = document.createElement('div');
-    card.classList.add('col-12');
-    card.innerHTML = '<h1>No ninjas encountered yet. Are you one? Feel free to add your card!</h1>';
-    cardParent.append(card);
-  }
-});
+)
